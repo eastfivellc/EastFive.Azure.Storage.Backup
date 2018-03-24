2ef6fe1591fbe15e47e93d42de4a52221479bc46
Scott Turner <turner-scott@tds.net>
2018-03-23 21:20:11 -0500

    update defaults

--- Blob/BlobCopyOptions.cs
+++ Blob/BlobCopyOptions.cs
@@ -10,15 +10,5 @@ namespace EastFive.Azure.Storage.Backup.Blob
         public TimeSpan maxIntervalCheckCopy;
         public TimeSpan maxTotalWaitForCopy;
         public int copyRetries;
-
-        public static readonly BlobCopyOptions Default = new BlobCopyOptions
-        {
-            accessPeriod = TimeSpan.FromMinutes(60),
-            maxConcurrency = 200,
-            minIntervalCheckCopy = TimeSpan.FromSeconds(1),
-            maxIntervalCheckCopy = TimeSpan.FromSeconds(15),
-            maxTotalWaitForCopy = TimeSpan.FromMinutes(5),
-            copyRetries = 5
-        };
     }
 }
